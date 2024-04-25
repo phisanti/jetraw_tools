8ac0a79bd9163c0533c93968b5ed2a031a19bef5
tisalon <tisalon@outlook.com>
2024-04-25 10:16:00 +0200

    move parser out

--- src/jetraw_tools/main.py
+++ src/jetraw_tools/main.py
@@ -1,61 +1,14 @@
 import os
 import re
-import argparse
 import locale
 import configparser
+from jetraw_tools.parser import parser
+from jetraw_tools import jetraw_tiff
 from jetraw_tools.compression_tool import CompressionTool
 from jetraw_tools.config import configjrt
 
 
 def main():
-    valid_extensions = [
-        ".nd2",
-        ".tif",
-        ".tiff",
-        ".ome.tif",
-        ".ome.tiff",
-        ".p.tif",
-        ".p.tiff",
-        ".ome.p.tif",
-        ".ome.p.tiff",
-    ]
-    parser = argparse.ArgumentParser(
-        prog="jetraw_tools", description="Compress images to JetRaw format"
-    )
-    parser.add_argument(
-        "-d", "--decompress", type=str, help="Path to file or folder to compress"
-    )
-    parser.add_argument(
-        "-c", "--compress", type=str, help="Path to file or folder to compress"
-    )
-    parser.add_argument(
-        "-s", "--settings", action="store_true", help="Initialize the configuration"
-    )
-    parser.add_argument(
-        "--calibration_file", type=str, default="", help="Path to calibration file"
-    )
-    parser.add_argument(
-        "--identifier", type=str, default="", help="Identifier for capture mode"
-    )
-    parser.add_argument(
-        "--extension",
-        type=str,
-        default=".tif",
-        choices=valid_extensions,
-        help="Image file extension",
-    )
-    parser.add_argument(
-        "--metadata", action="store_true", default=True, help="Process metadata"
-    )
-    parser.add_argument(
-        "--json", action="store_true", default=True, help="Save metadata as JSON"
-    )
-    parser.add_argument(
-        "--remove", action="store_true", default=False, help="Delete original images"
-    )
-    parser.add_argument(
-        "--verbose", action="store_true", default=True, help="Prints verbose output"
-    )
 
     # Pase and set locale
     args = parser.parse_args()
@@ -82,6 +35,10 @@ def main():
     else:
         identifier = args.identifier
 
+    if args.key == "":
+        licence_key = config["licence_key"]["key"]
+    jetraw_tiff._jetraw_tiff_lib.jetraw_tiff_set_license(licence_key.encode('utf-8'))
+
     if args.verbose:
         print(
             f"Using calibration file: {os.path.basename(cal_file)} and identifier: {identifier}"
@@ -102,16 +59,18 @@ def main():
         full_path = os.path.join(os.getcwd(), args.decompress)
         mode = "decompress"
         process_json = False
-    compressor = CompressionTool(cal_file, identifier, args.verbose)
-    compressor.process_folder(
-        full_path,
-        mode,
-        args.extension,
-        args.metadata,
-        ome_bool=True,
-        metadata_json=process_json,
-        remove_source=args.remove,
-    )
+    
+    if args.decompress or args.decompress:
+        compressor = CompressionTool(cal_file, identifier, args.verbose)
+        compressor.process_folder(
+            full_path,
+            mode,
+            args.extension,
+            args.metadata,
+            ome_bool=True,
+            metadata_json=process_json,
+            remove_source=args.remove,
+        )
 
 
 if __name__ == "__main__":
