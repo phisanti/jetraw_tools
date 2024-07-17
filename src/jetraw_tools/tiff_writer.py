01f95911929ce9523a16914908215615bc77566c
tisalon <tisalon@outlook.com>
2024-07-17 15:40:18 +0200

    handle error while writting json

--- src/jetraw_tools/tiff_writer.py
+++ src/jetraw_tools/tiff_writer.py
@@ -160,15 +160,15 @@ def metadata_writer(
     """
 
     if as_json:
-        if isinstance(metadata, ome_types.OME):
-            metadata_dump = json.loads(metadata.json())
-            json_filename = output_tiff_filename.replace(".ome.p.tiff", ".json")
-        else:
-            metadata_dump = metadata
-            json_filename = output_tiff_filename.replace(".p.tiff", ".json")
-
+        json_filename = output_tiff_filename.replace(".ome.p.tiff" if isinstance(metadata, ome_types.OME) else ".p.tiff", ".json")
+        
+        try:
+            metadata_dump = json.loads(metadata.json()) if isinstance(metadata, ome_types.OME) else metadata
+        except Exception:
+            metadata_dump = convert_to_ascii(metadata.dict() if isinstance(metadata, ome_types.OME) else metadata)
+        
         with open(json_filename, "w", encoding="utf-8") as f:
-            json.dump(metadata_dump, f, indent=3, ensure_ascii=False)
+            json.dump(metadata_dump, f, indent=3, ensure_ascii=False, default=serialise)
 
     if ome_bool:
         if isinstance(metadata, ome_types.OME):
