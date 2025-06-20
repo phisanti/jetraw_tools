ab7529a0b124d8ef72098104fb0ab889feae4746
Cano-Muniz, Santiago <tisalon@outlook.com>
2025-06-20 11:18:41 +0200

    re-order docs/conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,24 +1,17 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../../src"))
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+# Project information
 project = "jetraw_tools"
 copyright = "2025, Boeck Lab"
 author = "Boeck Lab"
 release = "0.7.0"
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+# General configuration
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
@@ -29,38 +22,21 @@ extensions = [
 
 templates_path = ["_templates"]
 exclude_patterns = []
-
 language = "en"
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# HTML output options
 html_theme = "furo"
 html_static_path = ["_static"]
 
-# -- Options for autodoc extension ------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#configuration
-
-# Automatically extract typehints when specified and place them in
-# descriptions of the relevant function/method.
+# Autodoc configuration
 autodoc_typehints = "description"
-
-# Don't show class signature with the class name.
 autodoc_class_signature = "separated"
-
-# This value contains a list of modules to be mocked up.
 autodoc_mock_imports = ["dpcore"]
-
-# Include both the class docstring and the __init__ method docstring
-autoclass_content = "both"
-
-# Generate stub pages for autosummary
+autoclass_content = 'class'
 autosummary_generate = True
 
-# Suppress warnings about duplicate object descriptions
+# Warning suppression
 suppress_warnings = ["autosummary.import_cycle", "autodoc.import_object"]
-
-# Don't fail on warnings (optional - removes -W flag behavior)
 nitpicky = False
 
 # Autodoc default options
